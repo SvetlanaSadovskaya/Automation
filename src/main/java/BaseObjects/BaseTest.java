93dd19780b06e1bec4f31a18010bb2b664888e36
SvetlanaSadovskaya <prosperity.piligrim@gmail.com>
2022-02-21 00:06:20 +0100

    homework 8 + 4

--- src/main/java/BaseObjects/BaseTest.java
+++ src/main/java/BaseObjects/BaseTest.java
@@ -1,27 +1,42 @@
 package BaseObjects;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import static BaseObjects.DriverCreation.closeDriver;
 import static BaseObjects.DriverCreation.getDriver;
+import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 public class BaseTest {
-    protected WebDriver driver;
-    protected ITestContext context;
+        protected WebDriver driver;
+        protected ITestContext context;
+        private String browserName;
 
-    @BeforeTest
-    public void precondition(ITestContext context) {
-        this.context = context;
-        this.driver = getDriver(context.getSuite().getParameter("browser"));
+        @BeforeTest
+        public void precondition(ITestContext context) {
+            BasicConfigurator.configure();
+            String log4jConfPath = "src/main/resources/log4j.properties";
+            PropertyConfigurator.configure(log4jConfPath);
+
+            this.context = context;
+            this.driver = getDriver(CHROME);
+            this.browserName = context.getSuite().getParameter("browser") == null ? System.getProperty("browser") : context.getSuite().getParameter("browser");
+            this.driver = getDriver(browserName == null ? "CHROME" : browserName);
+        }
+
+        @AfterTest
+        public void postcondition() {
+            closeDriver(CHROME);
+            closeDriver(browserName == null ? "CHROME" : browserName);
+        }
     }
 
-    @AfterTest
-    public void postcondition() {
-        closeDriver(context.getSuite().getParameter("browser"));
-    }
 
-}
 
---
+++ src/test/java/Lecture_8/HW8.java
@@ -0,0 +1,54 @@
+package Lecture_8;
+
+import BaseObjects.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HW8 extends BaseTest {
+
+    @Test(priority = 1)
+    public void checkList() {
+        driver.get("C:\\Sveta TMS\\Automation\\src\\test\\java\\Lecture_8\\Lecture_8.html");
+        List<String> firstColVals = new ArrayList<>();
+
+        firstColVals.add(driver.findElement(By.id("c1")).getText());
+        firstColVals.add(driver.findElement(By.id("c2")).getText());
+        firstColVals.add(driver.findElement(By.id("c3")).getText());
+        firstColVals.add(driver.findElement(By.id("c4")).getText());
+        firstColVals.add(driver.findElement(By.id("c5")).getText());
+        for (String el :
+                firstColVals) {
+            System.out.println(el);
+        }
+    }
+
+    @Test(priority = 2)
+    public void checkValueOn() {
+        // ввести значение в поле ввода
+        driver.findElement(By.id("c1")).sendKeys("Америка");
+        driver.findElement(By.id("c2")).sendKeys("Австралия");
+        // кликнуть чекбокс
+        driver.findElement(By.id("checkbox")).click();
+        // кликнуть на кнопку
+        driver.findElement(By.id("button")).click();
+        // выбрать элемент из списка
+        WebElement PartsOfTheWorld = driver.findElement(By.id("parts"));
+        Select selectPart = new Select(PartsOfTheWorld);
+        selectPart.selectByValue("Америка");
+        driver.findElement(By.id("c1")).click();
+        // проверить, что картинка существует
+        Assert.assertTrue(driver.findElement(By.name("blablabla")).isEnabled());
+        //проверить текст тега p
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "С вами свяжется наш менеджер.");
+        // кликнуть на ссылку и проверить, что новая страница была открыта
+        driver.findElement(By.tagName("a")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://localhost:63342/Automation/Lecture_8/Lecture_8.html");
+
+    }
+}
